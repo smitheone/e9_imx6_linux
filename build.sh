e7ff189379b0f01cac6082d0508e79f2c8314628
yimeng <yimeng.chn@gmail.com>
2015-11-24 11:32:39 +0800

    modified the build.sh for adding clean function.
    Signed-off-by: yimeng <yimeng.chn@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 CPU_JOB_NUM=$(($(grep processor /proc/cpuinfo | awk '{field=$NF};END{print field+1}')*3))
 echo "CPU number is "$CPU_JOB_NUM
 
+#cp config_e9_linux .config
+
 export ARCH=arm
-export CROSS_COMPILE=/opt/EmbedSky/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/tq-linaro-toolchain/bin/arm-none-linux-gnueabi-
-cp config_e9_linux .config
-make uImage -j$CPU_JOB_NUM
+export CROSS_COMPILE=~/toolchain/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/tq-linaro-toolchain/bin/arm-none-linux-gnueabi-
+
+if [ "clean" == "$1" ]; then
+	make clean
+else
+	make menuconfig
+	make uImage -j$CPU_JOB_NUM
+fi
 
